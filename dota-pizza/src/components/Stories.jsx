d91052716dd79a00c8c67afc158d7a69ade6c726
DyadyaNadya <146691114+DyadyaNadya@users.noreply.github.com>
2026-01-01 15:46:01 +0500

    Ф

--- dota-pizza/src/components/Stories.jsx
+++ dota-pizza/src/components/Stories.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import pudge from "../images/Pudge.png"
 import leftarrow from "../images/left-chevron.png"
 import rightarrow from "../images/chevron.png"
 import '../styles/stories.css'
 
 export default function Stories(){
+    const containerRef = useRef(null)
+    const [canScrollLeft, setCanScrollLeft] = useState(false)
+    const [canScrollRight, setCanScrollRight] = useState(true)
+
+    function checkScroll(){
+        const {scrollLeft, scrollWidth, clientWidth} = containerRef.current
+        canScrollLeft(scrollLeft > 0)
+        canScrollRight(scrollLeft < scrollWidth - clientWidth)
+    }
+    
+    
     return(
         <>
             <div className="stories-wrapper">
                 <LeftArrow imageSrc={leftarrow} size={70}/>
-            <div className="stories">
+            <div className="stories" ref={containerRef} onScroll={checkScroll}>
                 <Story imageSrc= {pudge}/>
                 <Story imageSrc= {pudge}/>
                 <Story imageSrc= {pudge}/>
@@ -40,12 +51,12 @@ function Story({imageSrc}){
 
 function LeftArrow({imageSrc, size}){
     return(
-        <img src={imageSrc} alt="" height={size} width={size}/>
+        <img src={imageSrc} alt="" height={size} width={size} style={{cursor: 'pointer'}}/>
     )
 }
 
 function RightArrow({imageSrc, size}){
     return(
-        <img src={imageSrc} alt="" height={size} width={size} />
+        <img src={imageSrc} alt="" height={size} width={size} style={{cursor: 'pointer'}}/>
     )
 }
